a0c3ebe005d6a46fe1a56adcc86639cf68595703
Zhe Sun <sunzhe1996@gmail.com>
2023-01-11 00:00:23 +0800

    proto

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import { readWrite } from "./web3.js";
 
-readWrite(58293).then(console.log);
+const from = 58293 - 600;
+const to = 58293;
+
+function main() {
+  for (let i = from; i <= to; i++) {
+    readWrite(i).then(console.log);
+  }
+}
+
+main();
